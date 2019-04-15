dfaff091224227b15e2eb736c7a04f660a15e5fd
Victor Chimenti <chimenti@seattleu.edu>
2019-04-15 07:20:02 -0700

    added check_valid and check_invalid handwritten unit tests

--- get_time.py
+++ get_time.py
@@ -25,8 +25,28 @@ def getdate(date_in):
     return True
 
 
+def check_valid():
+    try:
+        result = getdate('01-01')
+        if result is True:
+            print('01-01 Passed : Expected Result True : Actual Result True')
+    except ValueError:
+        print('01-01 Failed : Expected Result True : Actual Result False')
+
+
+def check_invalid():
+    try:
+        result = getdate('00-00')
+        if result is False:
+            print('00-00 Passed : Expected Result False : Actual Result False')
+    except ValueError:
+        print('01-01 Failed : Expected Result False : Actual Result True')
+
+
 date_input = input("Enter any month and day in the format MM-DD: ")
 date_today = datetime.datetime.now()
 print("Current Date/Time: ")
 print(str(date_today))
 print(str(getdate(date_input)))
+check_valid()
+check_invalid()
