93ef90a0a0c4b66e52c0e19a329b00bd0a71a463
Vic Chimenti <chimenti@seattleu.edu>
2019-04-14 19:47:35 -0700

    initial

--- get_time.py
+++ get_time.py
@@ -5,6 +5,12 @@
 #   A program to unit test a valid date format
 
 
+"""
+    This program uses the datetime library to validate user input for correct date entry.
+    The user is only required to enter Month and Day so the program will not account for leap years.
+"""
+
+
 import datetime
 
 
@@ -12,12 +18,14 @@ def getdate(date_in):
     try:
         datetime.datetime.strptime(date_in, '%m-%d')
     except ValueError:
-        print('Invalid Date Format, enter format YYYYY-MM-DD')
+        print('Invalid Date Format, enter format MM-DD')
         return False
+    print("You entered a valid date")
     return True
 
 
 date_input = input("Enter any month and day in the format MM-DD: ")
 date_today = datetime.datetime.now()
+print("Current Date/Time: ")
 print(str(date_today))
 print(str(getdate(date_input)))
